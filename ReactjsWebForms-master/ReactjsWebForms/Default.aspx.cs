426d9e7200212e5105f7e28ed7f744278c00ffeb
kgajjar <rhasquad@gmail.com>
2021-09-08 13:46:13 +0200

    Code Commit

---
+++ ReactjsWebForms-master/ReactjsWebForms/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using React;
+
+namespace ReactjsWebForms
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var env = AssemblyRegistration.Container.Resolve<IReactEnvironment>();
+            var objectModel = new { user = "React Usersssssss" };
+            var reactComponent = env.CreateComponent("PageContent", objectModel);
+
+            PageContent.Text = reactComponent.RenderHtml();
+        }
+    }
+}
---
+++ ReactjsWebForms-master/ReactjsWebForms/Scripts/components/helloWorld.jsx
@@ -0,0 +1,10 @@
+﻿var PageContent =  React.createClass({
+	render: function() {	
+		return (
+			<div>
+				<h2>My test</h2>
+				Hello world from {this.props.user}
+			</div>
+		);
+    }	
+});
